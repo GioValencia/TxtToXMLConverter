c1c8382b684802c554e1fb12c1f4b33604f950b9
GiovanniValencia <GiovanniValencia@GBTMJR2>
2019-01-28 13:51:40 -0600

    Fixed errors

--- TxtToXMLConverter/Program.cs
+++ TxtToXMLConverter/Program.cs
@@ -1,21 +1,76 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace TxtToXMLConverter
+public class ReadFile
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+    public string filePath;
+    public string[] content;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+    public ReadFile(string path)
+    {
+        //filePath = path;
+        content = System.IO.File.ReadAllLines(path);
+    }
+
+}
+
+public class XMLFile
+{
+    private string saveLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "_TXTinXMLFormat.xml");
+    private string[] txtDoc;
+    private string[] XMLArr;
+
+    public XMLFile(ReadFile file)
+    {
+        //saveLocation = Path.Combine(file.filePath, "_TXTinXMLFormat.txt");
+        txtDoc = file.content;
+    }
+
+    //Saves XML file at location specified, currently set to Desktop by default
+    public void saveXML(string[] XMLarr)
+    {
+        System.IO.File.WriteAllLines(saveLocation, XMLArr);
+    }
+
+    //Changes the txtDoc array to a format readable in XML
+    public void formatFile()
+    {
+        //Need to double check exact formatting for the test files before i include anything
+
+        //Grab txtDoc
+        //change each line to have appropriate labels in XML format   (delimiter == " ") (if (txtDoc[0].contains("[START]")))
+        //if makes easier, make different arrays for each label
+        //make another string array adding each previous line in the order they will be printed
+
+
+        saveXML(XMLArr);
+    }
+
+    public void spitTags()
+    {
+        List<string> tags = new List<string>();
+        //reading rates and nothing else
+        foreach (string s in txtDoc)
+        {
+            if (s.Contains("[START]")/*or anything else you wanna skip*/)
+            {
+
+            }
+            else if (s.Contains("RATEGROUP"))//Stopping point
+            {
+                break;
+            }
+            else
+            {
+                tags.Add(s);//adds line to list for later
+            }
         }
     }
+
+    public static void main()
+    {
+
+    }
+
 }
